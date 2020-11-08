6ad29bb61de48b8f3698520a7ac4906422fa7ef7
KevinMendes <mendes.kevin35@gmail.com>
2020-11-08 10:47:45 +0100

    first test update

--- re-app/src/App.test.tsx
+++ re-app/src/App.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders without crashing', () => {
+test('Render fonctionnel', () => {
   const { baseElement } = render(<App />);
   expect(baseElement).toBeDefined();
 });
